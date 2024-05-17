e8b1c61ec6668e041b3174e21cf64884418678ef
Viktoriia Shvets <vika.shvets1999@gmail.com>
2024-05-17 17:43:24 +0300

    Create initial view for uploaded files page

--- src/components/fileCard/FileCard.tsx
+++ src/components/fileCard/FileCard.tsx
@@ -1,5 +1,5 @@
 import {File} from "../../interfaces/File";
-import {Card, CardActions, CardContent, Collapse, IconButton, Typography} from "@mui/material";
+import {Box, Card, CardContent, Collapse, Divider, Grid, IconButton, Typography} from "@mui/material";
 import {useState} from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
@@ -11,44 +11,57 @@ interface Props  {
 
 function FileList({ file }: Props) {
     const [open, setOpen] = useState(false);
+    const propsToDisplay = ['fileType', 'fileSize', 'compressRatio'];
 
     return (
-            <Card sx={{ minWidth: 275 }} key={file.id}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        {file.fileName}
-                    </Typography>
-                    <Typography variant="body2">
-                        {file.fileType}
-                    </Typography>
-                    <Typography variant="body2">
-                        {file.fileSize}
-                    </Typography>
-                    <CardActions>
-                        <IconButton
-                            aria-label="expand row"
-                            size="small"
-                            onClick={() => setOpen(!open)}
-                        >
-                            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                        </IconButton>
-                        <Collapse in={open} timeout="auto" unmountOnExit>
+        <Card
+            key={file.id}
+            sx={{borderRadius: '4px', border: '1px solid rgba(0, 0, 0, 0.1)'}}
+        >
+            <Typography fontWeight={'600'} textTransform={'capitalize'} pt='2' fontSize='xs' padding={2}>
+                {file.fileName}
+            </Typography>
+            <CardContent>
+                <Grid
+                    gridTemplateColumns={'repeat(auto-fill, minmax(100px, 1fr))'}
+                    gap={2}
+                >
+                    <Box padding={'0px 8px 24px 8px'}>
+                    {propsToDisplay.filter(prop => prop !== "name").map((item:string) => (
+                        <Box key={item} alignItems={'center'} display={'flex'}>
+                            <Typography fontWeight={'600'} textTransform={'capitalize'} key={item} pt='2' fontSize='xs'>
+                                {`${item}:`}
+                            </Typography>
+                            <Typography paddingLeft={1} pt='2' fontSize='xs' fontWeight={'200'}>
+                                {/*@ts-ignore*/}
+                                { file[item]}
+                            </Typography>
+                        </Box>
+                    ))}
+                    </Box>
+                    <Divider/>
+                    <Box sx={{display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-start'}}>
+                        <Collapse in={open} timeout="auto" unmountOnExit sx={{padding: '16px 8px 0px 8px'}}>
                             <Typography variant="body2">
                                 {file.compressedFileData ?
-                                    'Compressed file is ready for download' :
+                                    `Compressed file is ready for download, compressed file size - ${file.compressedFileSize}` :
                                     'Processing in progress: compressed file will be available for download soon.'}
-                            </Typography>
-                            <Typography variant="body2">
-                                {file.compressedFileData ? file.compressedFileSize : 'Processing in progress: compressed file will be available for download soon.'}
                             </Typography>
                             {file.compressedFileData &&
                                 <a download={file.fileName} href={`${file.compressedFileData}`}><DownloadIcon/></a>
                             }
                         </Collapse>
-                    </CardActions>
-                </CardContent>
-            </Card>
-    )
+                        <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                    </Box>
+                </Grid>
+            </CardContent>
+        </Card>)
 }
 
 export default FileList;
--- src/components/fileList/FileList.tsx
+++ src/components/fileList/FileList.tsx
@@ -1,7 +1,16 @@
-import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
 import {getFileList} from "../../api";
 import {File} from "../../interfaces/File";
 import FileCard from "../fileCard/FileCard";
+import {
+    Grid,
+    Table,
+    Typography,
+    TableHead,
+    TableBody, TableRow,
+    TableCell,
+    useMediaQuery
+} from "@mui/material";
 
 interface Props  {
     setSnackbarOption: Dispatch<SetStateAction<{}>>
@@ -9,6 +18,39 @@ interface Props  {
 
 function FileList({ setSnackbarOption }: Props) {
     const [files, setFiles] = useState<File[] | null>(null);
+    const mobileView = useMediaQuery('(max-width: 1100px)');
+
+    const propsToDisplay = ['fileType', 'fileSize', 'compressRatio'];
+
+    const fileTile = <Grid
+        gridTemplateColumns={'repeat(auto-fill, minmax(250px, 1fr))'}
+    >
+        {files?.map((file: File) => (
+            <FileCard file={file} key={file.id}/>
+        ))}
+    </Grid>
+
+    const filesTable =
+        <Table sx={{maxWidth: '1000px'}}>
+            <TableHead>
+                <TableRow>
+                    {propsToDisplay.map((item: string) => (
+                        <TableCell key={item}>{item}</TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {files?.map((file: File) => (
+                    <TableRow key={file.id}>
+                        {propsToDisplay.map((item: string) => (
+                            // @ts-ignore
+                            <TableCell key={item}>{file[item]}</TableCell>
+                        ))}
+
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>;
 
     useEffect(() => {
         getFileList().then((response) => {
@@ -20,7 +62,10 @@ function FileList({ setSnackbarOption }: Props) {
 
     return (
         <>
-            {files && files.map((file: File) => (<FileCard file={file} key={file.id}/>))}
+            <Typography variant="h4" component="h1" align="center" margin={'32px 16px'} fontFamily={'"Raleway", cursive'} >
+               Already uploaded files
+            </Typography>
+            {mobileView ? fileTile : filesTable}
         </>
     );
 }
